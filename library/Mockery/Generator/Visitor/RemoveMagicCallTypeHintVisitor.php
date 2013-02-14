ffd344c2a5adea49faead1f2a6c935ac2a4f3347
Dave Marshall <dave.marshall@atstsolutions.co.uk>
2013-02-14 11:51:12 +0000

    Qualify class names

--- library/Mockery/Generator/Visitor/RemoveMagicCallTypeHintVisitor.php
+++ library/Mockery/Generator/Visitor/RemoveMagicCallTypeHintVisitor.php
@@ -5,10 +5,9 @@ namespace Mockery\Generator\Visitor;
 class RemoveMagicCallTypeHintVisitor extends \PHPParser_NodeVisitorAbstract
 {
     public function leaveNode(\PHPParser_Node $node) {
-        if ($node instanceof PHPParser_Node_Stmt_ClassMethod && $node->name == "__call") {
+        if ($node instanceof \PHPParser_Node_Stmt_ClassMethod && $node->name == "__call") {
             $params = $node->params;
             $params[1]->type = null;
         }
-
     }
 }
